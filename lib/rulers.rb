48044e1abbc53f9ba80795f3060e1919745b0612
noj33 <jonfriese33@gmail.com>
2013-11-04 22:12:20 -0800

    gem for rebuilding rails

---
+++ lib/rulers.rb
@@ -0,0 +1,34 @@
+require "rulers/version"
+require "rulers/routing"
+
+module Rulers
+  class Application
+    def call(env)
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404,
+          {'Content-Type' => 'text/html'}, []]
+      end
+
+      if env['PATH_INFO'] == '/'
+        return [200,
+          {'Content-Type' => 'text/html'}, ["Hello world!"]]
+      end
+
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+        [200, {'Content-Type' => 'text/html'},
+          [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
+    end
+  end
+end
